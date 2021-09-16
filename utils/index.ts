755514c99830ef415b724393097cf85bb83ffcfe
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-09-16 14:03:18 +0200

    chore: Add CDN URI to env

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
-export const CONTENT_DELIVERY_NETWORK_URI = "https://static-nft.pancakeswap.com";
+export const CONTENT_DELIVERY_NETWORK_URI =
+  process.env.CONTENT_DELIVERY_NETWORK_URI ?? "https://static-nft.pancakeswap.com";
 
 export const getTokenURI = (tokenUri: string): string => {
   if (tokenUri.startsWith("ipfs://")) {
