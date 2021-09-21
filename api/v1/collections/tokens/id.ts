9259fb55fa84181aae3325b0a31596e04179500e
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-09-21 16:46:23 +0200

    feat: Fetch token metadata on-the-fly if missing

--- api/v1/collections/tokens/id.ts
+++ api/v1/collections/tokens/id.ts
@@ -1,10 +1,14 @@
+import { Contract } from "@ethersproject/contracts";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { getAddress, isAddress } from "ethers/lib/utils";
 import { Attribute, Token } from "../../../../utils/types";
 import { getModel } from "../../../../utils/mongo";
-import { CONTENT_DELIVERY_NETWORK_URI, NETWORK } from "../../../../utils";
+import { CONTENT_DELIVERY_NETWORK_URI, getTokenURI, NETWORK } from "../../../../utils";
 import { Collection } from "mongoose";
 import { paramCase } from "param-case";
+import axios from "axios";
+import provider from "../../../../utils/provider";
+import ercABI from "../../../../utils/abis/ERC721.json";
 
 export default async (req: VercelRequest, res: VercelResponse): Promise<VercelResponse | void> => {
   if (req.method?.toUpperCase() === "OPTIONS") {
@@ -30,40 +34,76 @@ export default async (req: VercelRequest, res: VercelResponse): Promise<VercelRe
         .populate(["parent_collection", "metadata", "attributes"])
         .exec();
       if (!token) {
-        return res.status(404).json({ error: { message: "Entity not found." } });
+        // TO-DO: Implement an on-the-fly logic to get a token data related to a collection from its `tokenURI`.
+        const contract = new Contract(address, ercABI, provider);
+        const tokenURI = await contract.tokenURI(id);
+
+        const response = await axios(getTokenURI(tokenURI));
+
+        const data = {
+          tokenId: id,
+          name: response.data.name,
+          description: response.data.description,
+          image: {
+            original: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(address)}/${paramCase(
+              response.data.name
+            )}.png`,
+            thumbnail: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(address)}/${paramCase(
+              response.data.name
+            )}-1000.png`,
+            mp4: null,
+            webm: null,
+            gif: null,
+          },
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString(),
+          attributes: response.data.attributes
+            ? Object.keys(response.data.attributes).map((attribute: any) => ({
+                traitType: attribute,
+                value: response.data.attributes[attribute],
+                displayType: null,
+              }))
+            : [],
+          collection: {
+            name: "Pancake Bunnies",
+          },
+        };
+
+        return res.status(200).json({ data });
+      } else {
+        const data = {
+          tokenId: id,
+          name: token.metadata.name,
+          description: token.metadata.description,
+          image: {
+            original: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(address)}/${paramCase(
+              token.metadata.name
+            )}.png`,
+            thumbnail: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(address)}/${paramCase(
+              token.metadata.name
+            )}-1000.png`,
+            mp4: null,
+            webm: null,
+            gif: null,
+          },
+          createdAt: token.created_at,
+          updatedAt: token.updated_at,
+          attributes: token.attributes
+            ? token.attributes.map((attribute: Attribute) => ({
+                traitType: attribute.trait_type,
+                value: attribute.value,
+                displayType: attribute.display_type,
+              }))
+            : [],
+          collection: {
+            name: token.parent_collection.name,
+          },
+        };
+
+        return res.status(200).json({ data });
       }
-
-      const data = {
-        tokenId: id,
-        name: token.metadata.name,
-        description: token.metadata.description,
-        image: {
-          original: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(address)}/${paramCase(
-            token.metadata.name
-          )}.png`,
-          thumbnail: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(address)}/${paramCase(
-            token.metadata.name
-          )}-1000.png`,
-          mp4: null,
-          webm: null,
-          gif: null,
-        },
-        createdAt: token.created_at,
-        updatedAt: token.updated_at,
-        attributes: token.attributes
-          ? token.attributes.map((attribute: Attribute) => ({
-              traitType: attribute.trait_type,
-              value: attribute.value,
-              displayType: attribute.display_type,
-            }))
-          : [],
-        collection: {
-          name: token.parent_collection.name,
-        },
-      };
-
-      return res.status(200).json({ data });
     } catch (error) {
+      console.log(error);
       return res.status(500).json({ error: { message: "Unknown error." } });
     }
   }
--- utils/provider.ts
+++ utils/provider.ts
@@ -1,5 +1,20 @@
 import { providers } from "ethers";
+import sample from "lodash/sample";
+import { NETWORK } from ".";
 
-const provider = new providers.StaticJsonRpcProvider("https://data-seed-prebsc-2-s2.binance.org:8545/", 97);
+const MAINNET_RPC = [
+  "https://bsc-dataseed.binance.org/",
+  "https://bsc-dataseed1.defibit.io/",
+  "https://bsc-dataseed1.ninicoin.io/",
+];
+
+const TESTNET_RPC = [
+  "https://data-seed-prebsc-1-s1.binance.org:8545/",
+  "https://data-seed-prebsc-1-s2.binance.org:8545/",
+  "https://data-seed-prebsc-2-s1.binance.org:8545/",
+  "https://data-seed-prebsc-2-s2.binance.org:8545/",
+];
+
+const provider = new providers.JsonRpcProvider(NETWORK === "testnet" ? sample(TESTNET_RPC) : sample(MAINNET_RPC));
 
 export default provider;
