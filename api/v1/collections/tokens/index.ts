7786062f39d771d37842e41ed19241a5eb091160
RabbitDoge <72658581+RabbitDoge@users.noreply.github.com>
2021-09-23 15:25:09 +0900

    chore: Rework bunny id system

--- api/v1/collections/tokens/index.ts
+++ api/v1/collections/tokens/index.ts
@@ -1,17 +1,31 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import { isAddress, getAddress } from "ethers/lib/utils";
-import reduce from "lodash/reduce";
 import get from "lodash/get";
-import { Collection } from "mongoose";
 import { CONTENT_DELIVERY_NETWORK_URI, NETWORK } from "../../../../utils";
-import { Attribute, Token } from "../../../../utils/types";
+import { Attribute, Token, Collection } from "../../../../utils/types";
 import { getModel } from "../../../../utils/mongo";
 import { paramCase } from "param-case";
 
-const formatGenericList = (tokens: Token[], address: string) => {
+const PANCAKE_BUNNY_ADDRESS = process.env.PANCAKE_BUNNY_ADDRESS as string;
+
+/**
+ * Fetch tokens from a generic collection
+ * @param collection
+ * @returns
+ */
+const fetchGeneric = async (collection: Collection) => {
+  // Fetch
+  const tokenModel = await getModel("Token");
+  const tokens: Token[] = await tokenModel
+    .find({ parent_collection: collection })
+    .populate(["parent_collection", "metadata", "attributes"])
+    .exec();
+
+  // Format
   let data = {};
   const attributesDistribution: { [key: string]: { [key: string]: number } } = {};
   tokens.forEach((token: Token) => {
+    const metaName = paramCase(token.metadata.name);
     data = {
       ...data,
       [token.token_id]: {
@@ -19,12 +33,10 @@ const formatGenericList = (tokens: Token[], address: string) => {
         name: token.metadata.name,
         description: token.metadata.description,
         image: {
-          original: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(address)}/${paramCase(
-            token.metadata.name
-          )}.png`,
-          thumbnail: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(address)}/${paramCase(
-            token.metadata.name
-          )}-1000.png`,
+          original: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(collection.address)}/${metaName}.png`,
+          thumbnail: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(
+            collection.address
+          )}/${metaName}-1000.png`,
           mp4: null,
           webm: null,
           gif: null,
@@ -57,44 +69,86 @@ const formatGenericList = (tokens: Token[], address: string) => {
       attributesDistribution[traitType][traitValue] += 1;
     });
   }); // End forEach
+
   return { data, attributesDistribution };
 };
 
-const formatBunnies = (tokens: Token[], address: string) => {
-  let data: { [key: string]: any } = {};
-  tokens.forEach((token: Token) => {
-    const bunnyId = token.attributes[0].value;
-    const exist = !!data[bunnyId];
+/**
+ * Fetch tokens from the pancake bunnies collection
+ * @param collection
+ * @returns
+ */
+const fetchPancakeBunnies = async (collection: Collection) => {
+  // Fetch
+  const tokenModel = await getModel("Token");
+  const attributeModel = await getModel("Attribute");
+  const attributes = await attributeModel.find({ parent_collection: collection });
 
-    if (exist) {
-      data[bunnyId].tokens.push(token.token_id);
-    } else {
-      data = {
-        ...data,
-        [bunnyId]: {
-          name: token.metadata.name,
-          description: token.metadata.description,
-          image: {
-            original: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(address)}/${paramCase(
-              token.metadata.name
-            )}.png`,
-            thumbnail: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(address)}/${paramCase(
-              token.metadata.name
-            )}-1000.png`,
-            mp4: null,
-            webm: null,
-            gif: null,
+  const promisesAttributesDistribution = attributes.map(async (attribute) => {
+    return await tokenModel
+      .aggregate([
+        {
+          $match: {
+            parent_collection: collection._id,
+            attributes: attribute._id,
           },
-          collection: {
-            name: token.parent_collection.name,
-          },
-          tokens: [token.token_id],
         },
-      };
+        {
+          $count: "token_id",
+        },
+      ])
+      .exec();
+  });
+
+  const promisesTokens = attributes.map(async (attribute) => {
+    const res = await tokenModel
+      .findOne({ parent_collection: collection, attributes: attribute })
+      .populate(["metadata", "attributes"])
+      .exec();
+
+    if (!res) {
+      return null;
     }
-  }); // End forEach
-  const attributesDistribution = reduce(data, (acc, value, index) => ({ ...acc, [index]: value.tokens.length }), {});
-  return { data, attributesDistribution: { bunnyId: attributesDistribution } };
+
+    const metaName = paramCase(res.metadata.name);
+    return {
+      name: res.metadata.name,
+      description: res.metadata.description,
+      image: {
+        original: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(PANCAKE_BUNNY_ADDRESS)}/${metaName}.png`,
+        thumbnail: `${CONTENT_DELIVERY_NETWORK_URI}/${NETWORK}/${getAddress(
+          PANCAKE_BUNNY_ADDRESS
+        )}/${metaName}-1000.png`,
+        mp4: null,
+        webm: null,
+        gif: null,
+      },
+      collection: {
+        name: collection.name,
+      },
+    };
+  });
+  const tokens = await Promise.all(promisesTokens);
+
+  // Format
+  const data: { [key: string]: Token } = attributes.reduce((acc, attribute: Attribute) => {
+    const bunnyId = parseInt(attribute.value, 10);
+    const token = tokens[bunnyId];
+    return {
+      ...acc,
+      [bunnyId]: token,
+    };
+  }, {});
+
+  const attributesDistribution = await Promise.all(promisesAttributesDistribution);
+
+  return {
+    data,
+    attributesDistribution: attributesDistribution.reduce(
+      (acc, value, index) => ({ ...acc, [index]: value[0] ? value[0].token_id : null }),
+      {}
+    ),
+  };
 };
 
 export default async (req: VercelRequest, res: VercelResponse): Promise<VercelResponse | void> => {
@@ -112,22 +166,16 @@ export default async (req: VercelRequest, res: VercelResponse): Promise<VercelRe
       return res.status(404).json({ error: { message: "Entity not found." } });
     }
 
-    const tokenModel = await getModel("Token");
-    const tokens: Token[] = await tokenModel
-      .find({ parent_collection: collection })
-      .populate(["parent_collection", "metadata", "attributes"])
-      .exec();
-    if (tokens.length === 0) {
+    const { data, attributesDistribution } =
+      address.toLowerCase() === PANCAKE_BUNNY_ADDRESS?.toLowerCase()
+        ? await fetchPancakeBunnies(collection)
+        : await fetchGeneric(collection);
+
+    const total = Object.keys(data).length;
+    if (total === 0) {
       return res.status(404).json({ error: { message: "Entity not found." } });
     }
-
-    const { data, attributesDistribution } =
-      address.toLowerCase() === process.env.PANCAKE_BUNNY_ADDRESS?.toLowerCase()
-        ? formatBunnies(tokens, address)
-        : formatGenericList(tokens, address);
-
-    const total = tokens.length;
-    return res.status(200).json({ total, attributesDistribution, data });
+    return res.status(200).json({ attributesDistribution, data, total });
   }
 
   return res.status(400).json({ error: { message: "Invalid address." } });
